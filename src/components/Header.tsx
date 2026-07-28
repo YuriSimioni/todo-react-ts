0191b29b386ce805f732b3bbe5d7752750c0d8a0
YuriSimioni <yurirs.contato@gmail.com>
2026-07-27 22:08:24 -0300

    feat: header page home is styled

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+  return (
+    <header className="w-screen">
+        <p className="bg-slate-600 p-8 text-slate-50 font-bold align-middle text-2xl text-center shadow-2xl">
+            TODO List React.js
+        </p>
+    </header>
+  );
+}
+
+export default Header;
